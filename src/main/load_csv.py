77deeeacdd1c28b6f32da5f64939b55c15c26869
Vartika Parashar <vparashar@qatarairways.com.qa>
2022-01-10 15:58:18 +0100

    added column is_age_above_25

--- src/main/load_csv.py
+++ src/main/load_csv.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 from pathlib import Path
 
 dir_path = Path(__file__).parent.parent
@@ -6,5 +7,6 @@ dir_path = Path(__file__).parent.parent
 spark = SparkSession.Builder().getOrCreate()
 df = spark.createDataFrame([("1","VP"), ("2", "AP")], ["id", "name"])
 df2 = spark.read.option("header", "true").csv(f"{dir_path}/resources/data.csv")
+df2 = df2.withColumn("is_age_above_25", F.when(F.col("age") > 25, "Yes").otherwise("No"))
 df2.show()
 df.show()
