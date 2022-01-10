6a14a30217b315934aea81e14aac16b01a0ceeb4
Vartika Parashar <vparashar@qatarairways.com.qa>
2022-01-10 15:53:15 +0100

    added csv file and df

--- src/main/load_csv.py
+++ src/main/load_csv.py
@@ -1,5 +1,10 @@
 from pyspark.sql import SparkSession
+from pathlib import Path
+
+dir_path = Path(__file__).parent.parent
 
 spark = SparkSession.Builder().getOrCreate()
 df = spark.createDataFrame([("1","VP"), ("2", "AP")], ["id", "name"])
+df2 = spark.read.option("header", "true").csv(f"{dir_path}/resources/data.csv")
+df2.show()
 df.show()
